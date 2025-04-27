0086c28a7e1c06875c7602c778293f88e88c221b
chaman0011 <121281059+chaman0011@users.noreply.github.com>
2025-04-27 17:19:53 +0530

    Update A4task2.py

--- A4task2.py
+++ A4task2.py
@@ -1,15 +1,15 @@
-file1=open("sample.txt","w")
+file1=open("output.txt","w")
 data=input("Enter the text to write to the file:")
 file1.write(data + '\n')
 print("Data successfully written to output.txt")
 file1.close()
 
-file1=open("sample.txt","a")
+file1=open("output.txt","a")
 data1=input("Enter the additional text to append:")
 file1.write(data1+'\n')
 file1.close()
 
 
-with open('sample.txt', 'r') as file:
+with open('output.txt', 'r') as file:
     for line in file:
         print(line, end='')
